f2b5268b70e6442b1cd267945420ce8d0264f404
ipeleng-bela <ipeleng.bela@gmail.com>
2019-07-02 21:44:44 +0200

    cotrollers

---
+++ scripts/controllers/nav.js
@@ -0,0 +1,16 @@
+'use strict';
+
+app.controller('NavController', function($scope, $location, toaster, Auth) {
+
+	$scope.currentUser = Auth.user;
+	$scope.signedIn = Auth.signedIn;
+	$scope.login = Auth.login;
+
+  $scope.logout = function() {    
+    Auth.logout();
+    $location.path('/');    
+    toaster.pop('success', "Logged out successfully");
+    
+  };
+	
+});
